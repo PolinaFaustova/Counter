f2044abfe8bfffdd84288c777066d8c2d9ec3132
PolinaFaustova <polinamusikerin@gmail.com>
2023-05-08 12:07:31 +0200

    Update Controls.jsx

--- src/components/Counter/Controls.jsx
+++ src/components/Counter/Controls.jsx
@@ -1,7 +1,8 @@
 // import React from 'react';
+// import css from './Counter.module.css'
 
 // export const Controls = ({ onIncrement, onDecrement }) => {
-//   <div className="Counter__controls">
+//   <div className={css.Counter__controls}>
 //     <button type="button" onClick={onIncrement}>
 //       Збільшити на один
 //     </button>
